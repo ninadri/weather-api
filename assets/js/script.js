a861f0cf042c612a98b611efff8eb9ec774095f5
ninadri <159666628+ninadri@users.noreply.github.com>
2024-06-09 15:27:24 -0500

    made recent searches button work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,120 @@
-
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
-let recentSearchEl = document.getElementById("recent-searches");
+const recentSearch = document.getElementById("recent-searches");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-
-const API_Key ="063dd6bdb3f51bc236d6d40ade5bf76d"; // API key for OpenWeatherMap API
-
+const API_Key = "063dd6bdb3f51bc236d6d40ade5bf76d"; // API key for OpenWeatherMap API
 
 const createWeatherCard = (cityName, weatherItem, index) => {
-    if(index === 0) { // HTML for the main weather card
-        return `<div class="details">
-                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-                    <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+  // reformatting date as MM-DD-YYYY
+  const date = new Date(weatherItem.dt_txt);
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  }).format(date);
+
+  if (index === 0) {
+    // HTML for the main weather card
+    return `<div class="details">
+                    <h2>${cityName} (${formattedDate})</h2>
+                    <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(
+                      2
+                    )}°C</h4>
                     <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
                     <h4>Humidity: ${weatherItem.main.humidity}%</h4>
                 </div>
                 <div class="icon">
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <img src="https://openweathermap.org/img/wn/${
+                      weatherItem.weather[0].icon
+                    }@4x.png" alt="weather-icon">
                     <h6>${weatherItem.weather[0].description}</h6>
                 </div>`;
-    } else { // HTML for the other five day forecast card
-        return `<li class="card">
-                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
-                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
+  } else {
+    // HTML for the other five day forecast card
+    return `<li class="card">
+                    <h3>(${formattedDate})</h3>
+                    <img src="https://openweathermap.org/img/wn/${
+                      weatherItem.weather[0].icon
+                    }@4x.png" alt="weather-icon">
+                    <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(
+                      2
+                    )}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </li>`;
-    }
-}
-
-const getWeatherDetails = (cityName, lat, lon) => {
-    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_Key}`;
-
-    fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
-        // Filter the forecasts to get only one forecast per day
-        const uniqueForecastDays = [];
-        const fiveDaysForecast = data.list.filter(forecast => {
-            const forecastDate = new Date(forecast.dt_txt).getDate();
-            if (!uniqueForecastDays.includes(forecastDate)) {
-                return uniqueForecastDays.push(forecastDate);
-            }
-        });
-
-        // Clears previous weather data
-        cityInput.value = "";
-        currentWeatherDiv.innerHTML = "";
-        weatherCardsDiv.innerHTML = "";
-
-        // Creates weather cards and adding them to the DOM
-        fiveDaysForecast.forEach((weatherItem, index) => {
-            const html = createWeatherCard(cityName, weatherItem, index);
-            if (index === 0) {
-                currentWeatherDiv.insertAdjacentHTML("beforeend", html);
-            } else {
-                weatherCardsDiv.insertAdjacentHTML("beforeend", html);
-            }
-        });        
-    })
-    };
-
-
-const getCityCoordinates = () => {
-
-    const cityName = cityInput.value.trim();
-    console.log(cityName);
-    if (cityName === "") return;
-    const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_Key}`;
-    
-    fetch(API_URL).then(response => response.json()).then(data => {
-        const { lat, lon, name } = data[0];
-        getWeatherDetails(name, lat, lon);
-        updateRecentSearches(name);
-    })
+  }
 };
 
+const getWeatherDetails = (cityName, lat, lon) => {
+  const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_Key}`;
 
-  function updateRecentSearches(cityName) {
-        if (!recentSearchEl.querySelector(`button[data-city="${cityName}"]`)) {
-            const button = document.createElement("button");
-            button.textContent = cityName;
-            button.className = 'location-btn'; 
-            button.setAttribute('data-city', cityName); 
-            button.style.display = 'block'; 
-            button.style.backgroundColor = 'grey'; 
-            button.style.color = 'white';
-            button.style.border = 'none';
-            button.style.padding = '10px';
-            button.style.marginTop = '5px';
-            button.onclick = () => getCityCoordinates(cityName);
-            recentSearchEl.appendChild(button);
-        }    
+  fetch(WEATHER_API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      // Filter the forecasts to get only one forecast per day
+      const uniqueForecastDays = [];
+      const fiveDaysForecast = data.list.filter((forecast) => {
+        const forecastDate = new Date(forecast.dt_txt).getDate();
+        if (!uniqueForecastDays.includes(forecastDate)) {
+          return uniqueForecastDays.push(forecastDate);
+        }
+      });
+
+      // Clears previous weather data
+      cityInput.value = "";
+      currentWeatherDiv.innerHTML = "";
+      weatherCardsDiv.innerHTML = "";
+
+      // Creates weather cards and adding them to the DOM
+      fiveDaysForecast.forEach((weatherItem, index) => {
+        const html = createWeatherCard(cityName, weatherItem, index);
+        if (index === 0) {
+          currentWeatherDiv.insertAdjacentHTML("beforeend", html);
+        } else {
+          weatherCardsDiv.insertAdjacentHTML("beforeend", html);
+        }
+      });
+    });
+};
+
+const getCityCoordinates = () => {
+  const cityName = cityInput.value.trim();
+  console.log(cityName);
+  if (cityName === "") return;
+  const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_Key}`;
+
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const { lat, lon, name } = data[0];
+      getWeatherDetails(name, lat, lon);
+      updateRecentSearches(name, lat, lon);
+    });
+};
+
+function updateRecentSearches(cityName, lat, lon) {
+  if (!recentSearch.querySelector(`button[data-city="${cityName}"]`)) {
+    const button = document.createElement("button");
+    button.textContent = cityName;
+    button.className = "location-btn";
+    button.setAttribute("data-city", cityName);
+    button.style.display = "block";
+    button.style.backgroundColor = "grey";
+    button.style.color = "white";
+    button.style.border = "none";
+    button.style.padding = "10px";
+    button.style.marginTop = "5px";
+
+    button.onclick = () => getWeatherDetails(cityName, lat, lon);
+
+    recentSearch.appendChild(button);
+  }
 }
 
 searchButton.addEventListener("click", getCityCoordinates);
-cityInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
+cityInput.addEventListener(
+  "keyup",
+  (e) => e.key === "Enter" && getCityCoordinates()
+);
